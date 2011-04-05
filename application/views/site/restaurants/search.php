954e99bce6c45a3bc5f8a81d647484a10d30032c
Omry Oz <omryoz@gmail.com>
2011-04-05 15:35:57 +0300

    start of search function

---
+++ application/views/site/restaurants/search.php
@@ -0,0 +1,9 @@
+<div id="restquery">
+	<?php echo Form::input('rest_name',0,array('id'=>'rest_name','type'=>'hidden'));?>
+    <?php echo Form::input('city_id',0,array('id'=>'city_id','type'=>'hidden'));?>
+	<?php echo Form::input('kitchen_type',0,array('id'=>'kitchen_type','type'=>'hidden'));?>
+    <?php echo Form::input('min_order',0,array('id'=>'min_order','type'=>'hidden'));?>
+    <?php echo Form::input('delivery_cost',0,array('id'=>'delivery_cost','type'=>'hidden'));?>
+	<?php echo Form::input('kosher_level',0,array('id'=>'kosher_level','type'=>'hidden'));?>
+	<?php echo Form::input('delivery_time',0,array('id'=>'delivery_time','type'=>'hidden'));?>
+</div>
